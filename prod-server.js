f0572085dcae9a0ce25a1cc634b1ba0c17734a73
nemesis-sun <nemesis.sun.now@gmail.com>
2016-03-04 23:45:10 +0800

    prepare master for heroku

--- prod-server.js
+++ prod-server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var path = require('path');
 
+var port =  process.env.PORT || 8080;
+
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -8,6 +10,6 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 });
 
-app.listen(8080, function() {
-  console.log('Express production server running at localhost:8080');
+app.listen(port, function() {
+  console.log('Express production server running at port 8080');
 });
